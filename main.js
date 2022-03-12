6596203eac0741f559d8b6635ab81a2fb2f0dd76
Erick Brito <erick261@gmail.com>
2022-03-12 10:46:20 +0100

    erick

--- main.js
+++ main.js
@@ -1,33 +1,29 @@
 import { preguntas } from "/preguntas.js";
 
-/// Hacer lectura de cada uno de los objetos y mostrarlos en el dom
+/// Hacer lectura de cada uno de los objetos
+const titulo = document.querySelector("h2")
+
+console.log(preguntas[0]);
+for(let i = 0; i<preguntas.length ; i++){
+    
+    console.log(preguntas[i])
+}
+
+/* for(const objetos of preguntas){
+    const pregunta = objetos.question
+    const respuestas = objetos.answers
+    const correcta = objetos.correct
+    titulo.textContent = pregunta
+    for(const objRespuestas of respuestas){     
+    }
+} */
+
+
 /// Hacer lectura de cada una de las respuestas y mostrarlas en el dom
+
 /// Tomar la respuesta seleccionada y validarla con la correcta
 //// Si la respuesta es correcta, contador sube 1, si no, se mantiene
 /// Al final, debe sumar el puntaje de todas las respuestas correctas
 
-//console.log(cuestionario);
 
-function quiz(respuesta){
-    for(const questionJSON of preguntas){
-    const respuestaCorrecta = questionJSON.correct;
-    const pregunta = questionJSON.question
-    const opciones = questionJSON.answers
-    let counter = 0
-
-    /* for(const answers of opciones){
-        console.log(answers);
-    }
-    console.log(opciones); */
-    if(respuesta === respuestaCorrecta){
-        counter++;
-        console.log(`Tu puntuacion es: ${counter}`);
-    } else {console.log("Siguiente pregunta");}
-    
-    
-    //console.log(`Pregunta: ${pregunta} y la respuesta correcta es ${respuestaCorrecta}`);
-   // console.log(`Pregunta: ${questionJSON.question}`);
-   
-}
-}
-quiz("Labrador Retriever")
+//Debo tomar 1 objeto, con su respuesta y su pregunta a la vez, luego meterla en una funcion para conseguir las 50 preguntas
